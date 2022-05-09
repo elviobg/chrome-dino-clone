74c58ab9e51717969706c1098bd264c3e155a437
Elvio Batista <elvio.batista@pebmed.com.br>
2022-05-09 18:47:36 -0300

    adiciona mecânica de pulo

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,1 +1,47 @@
-console.log('game.js');
+const game = {
+    dino: {
+        isJumping: false,
+        position: 0,
+    }
+}
+
+document.addEventListener('keyup', handleKeyUp);
+function handleKeyUp(event) {
+    if (event.keyCode === 32 && !game.dino.isJumping) {
+        jump();
+    }
+}
+
+function dinoUp() {
+    game.dino.isJumping = true;
+    let moves = 0;
+    let dinoUpInterval = setInterval(() => {
+        moves += 1;
+        game.dino.position += 20;
+        dino.style.bottom = game.dino.position + 'px';
+        if(moves >= 8) {
+            dinoDown();
+            clearInterval(dinoUpInterval);
+        }
+      }, 20);
+}
+
+function dinoDown() {
+    let moves = 0;
+    let dinoDownInterval = setInterval(() => {
+        moves += 1;
+        game.dino.position -= 20;
+        dino.style.bottom = game.dino.position + 'px';
+        if (moves >= 8) {
+          clearInterval(dinoDownInterval);
+          game.dino.isJumping = false;
+        }
+    }, 20);
+}
+
+function jump() {
+    if(!game.dino.isJumping) {
+        dinoUp();
+    }
+}
+  
